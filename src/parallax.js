a2840763d0e669deca2acf35cb1fcf8e967acb36
Jimmy Hillis <jimmy@hillis.me>
2013-01-16 15:59:39 +0800

    Initial working jQuery plugin

--- src/parallax.js
+++ src/parallax.js
@@ -9,11 +9,24 @@
 (function($) {
 
     // Collection method.
-    $.fn.parallax = function() {
+    $.fn.parallax = function(options) {
 
         var count = 0;
+        options = $.extend({
+            'speed': 3,
+            'direction': -1
+        }, options);
 
         return this.each(function() {
+
+            var $this = $(this)
+              , container_offset = $this.offset().top - 500;
+
+            $(window).scroll(function () {
+                var current_vertical_offset = $(document).scrollTop()
+                  , new_vertical_position = (current_vertical_offset - container_offset) / (options.direction * options.speed);
+                $this.css("background-position", "50% " + new_vertical_position + "px");
+            });
 
         });
 
